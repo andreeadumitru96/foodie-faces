8c22d75a453865ab711b52c0981ab5ac2506833a
Andreea <dumitru_andreea96@yahoo.com>
2018-03-29 12:03:44 +0200

    LoginContainer View added

---
+++ client/src/components/individual/Authentication/LoginContainer/Login/Login.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+import './Login.css';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        };
+    }
+
+    render() {
+        return (
+            <div className="login">
+                <form className="login__form">
+                    <div className="login__form-p">
+                        <p>LOG IN</p>
+                    </div>
+                    <div className="login__form-email">
+                        <TextField  
+                            inputStyle={{color: 'white'}}
+                            floatingLabelStyle={{color: 'white'}}
+                            floatingLabelText="Email"
+                            ref={(email) => { this.email = email }}
+                        />  
+                    </div>
+
+                    <div className="login__from-password">
+                        <TextField
+                            inputStyle={{color: 'white'}}
+                            floatingLabelStyle={{color: 'white'}}
+                            floatingLabelText="Password"
+                            // type="password"
+                            ref={(password) => { this.password = password }}
+                        />
+                    </div>
+
+                    <div className="login__form-sign-in-button">
+                        <RaisedButton label="LOG IN"
+                            onClick={this.props.onLogin}
+                        />
+                    </div>
+
+                    <div className="login__form-forgot-password">
+                        <a className="login__form-forgot-password-anchor"> Forgot your password? </a>
+                    </div>  
+                    <div className="login__redirect-register">
+                        <p> Don't you have an account? Register here </p>     
+                    </div>       
+                    
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login;
