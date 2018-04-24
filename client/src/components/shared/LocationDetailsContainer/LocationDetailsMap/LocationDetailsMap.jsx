2e7cfae4593cf45be0cd3ee9182bd16308f58625
Andreea <dumitru_andreea96@yahoo.com>
2018-04-24 10:50:55 +0200

    Client - Add a review on Location

---
+++ client/src/components/shared/LocationDetailsContainer/LocationDetailsMap/LocationDetailsMap.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class LocationDetailsMap extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+           <div className="location-details-map">
+                dasda
+           </div>
+
+        );
+    }
+
+}
+
+export default LocationDetailsMap;
--- client/src/components/shared/LocationDetailsContainer/LocationDetailsReviews/LocationDetailsReviews.jsx
+++ client/src/components/shared/LocationDetailsContainer/LocationDetailsReviews/LocationDetailsReviews.jsx
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import ReactStars from 'react-stars';
 
+import {cookies} from '../../../shared/constants';
+import {notificationError} from '../../../shared/constants';
+
+
+
 class LocationDetailsReviews extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            locationDetails: this.props.locationDetails
         };
+        
+        this.reviewScore = this.state.locationDetails.averageScore;
+        this._getReviewDetails = this._getReviewDetails.bind(this);
+        this._onAddReview = this._onAddReview.bind(this);
+        this._onRatingChanged = this._onRatingChanged.bind(this);
+        // this._parseDate = this._parseDate.bind(this);
     }
 
     render() {
         return (
             <div className="location-details-reviews-wrapper">
-                {this.props.locationDetails.receivedReviews.map((receivedReview) => (
+                {this.state.locationDetails.receivedReviews.map((receivedReview) => (
                     <div className="location-details-reviews">
                         <div className="location-details-reviews__user">
                             <div className="location-details-reviews__user-picture">
@@ -27,6 +39,7 @@ class LocationDetailsReviews extends Component {
                                 {receivedReview.score}
                             </div>
                             <div className="location-details-reviews-entity-date">
+                                {/* {this._parseDate(receivedReview.createdDate)} */}
                                 {receivedReview.createdDate}
                             </div>
                             <div className="location-details-reviews__entity-title">
@@ -40,20 +53,25 @@ class LocationDetailsReviews extends Component {
                 ))}
                 <div className="location-details-reviews__add-review">
                     <form>
-                        <fieldset class="location-details-reviews__add-review-components">
+                        <fieldset className="location-details-reviews__add-review-components">
                             <label> Add a review </label>
                             <ReactStars
                                 count={5}
                                 size={24}
                                 color2={'black'}
                                 half={false}
-                                value={this.props.locationDetails.tripAdvisorRating}
+                                value={this.state.locationDetails.averageScore}
+                                onChange={this._onRatingChanged}
                             />
-                            <input placeholder="Title" className="location-details-reviews__add-review-components-title"></input>
-                            <textarea class="main__standard-section-textarea" placeholder="Leave a review..." className="location-details-reviews__add-review-components-body"></textarea>
+                            <input className="location-details-reviews__add-review-components-title" placeholder="Title" 
+                                ref={(reviewTitle) => {this.reviewTitle = reviewTitle}}
+                            />
+                            <textarea className="location-details-reviews__add-review-components-body" placeholder="Leave a review..."
+                                ref={(reviewContent) => {this.reviewContent = reviewContent}}
+                            />
 
                             <div className="location-details-reviews__add-review-components-button">
-                                <button className="location-details-reviews__add-review-components-button-add" type="button"> <i class="fa fa-plus-square"></i> Add review </button>
+                                <button className="location-details-reviews__add-review-components-button-add" type="button" onClick={this._onAddReview}> <i className="fa fa-plus-square"></i> Add review </button>
                             </div>
                         </fieldset>
                     </form>
@@ -62,6 +80,64 @@ class LocationDetailsReviews extends Component {
 
         );
     }
+
+    _onRatingChanged(newRating) {
+        this.reviewScore = newRating;
+    }
+
+    _getReviewDetails() {
+       
+        let user = cookies.get('user');
+        console.log(user);
+
+        let reviewDetails = {
+            title: this.reviewTitle.value,
+            content: this.reviewContent.value,
+            score: this.reviewScore,
+            averageScore: this.state.locationDetails.averageScore,
+            userName: user.fullName,
+            userId: user._id,
+            locationId: this.state.locationDetails._id
+        };
+        return reviewDetails;
+    }
+
+    _onAddReview() {
+
+        let reviewDetails = this._getReviewDetails();
+
+        fetch('http://localhost:3001/api/location/addReview', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+
+            method: 'post',
+            body: JSON.stringify(reviewDetails)
+
+        }).then(function(response){
+            if(response.status === 200) {
+                response.json().then((location) => {
+                    this.setState({
+                        locationDetails: location
+                    });
+                    this.reviewTitle.value = '';
+					this.reviewContent.value = '';
+                })
+            } else {
+                response.json().then((error) => {
+                	notificationError(error.message);
+                })
+            }
+        }.bind(this))
+    }
+
+    // _parseDate(date) {
+    //     // let parsedDate = date.toLocaleDateString();
+    //     var utc = date.toISOString().split('T')[0];
+    //     return utc;
+
+    // }
 
 }
 
