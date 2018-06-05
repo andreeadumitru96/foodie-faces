cc29f7e84172d54f6695f8dbbdeff5021a54c2ef
Andreea <dumitru_andreea96@yahoo.com>
2018-06-05 23:08:03 +0200

    Client - Add pop-up full size image on click

--- client/src/components/shared/LocationDetailsContainer/LocatonDetailsGrid.jsx/LocationDetailsGrid.jsx
+++ client/src/components/shared/LocationDetailsContainer/LocatonDetailsGrid.jsx/LocationDetailsGrid.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { GridList, GridTile } from 'material-ui/GridList';
 
 import './LocationDetailsGrid.css';
+import FullSizeImage from './FullSizeImage/FullSizeImage';
 
 const styles = {
     root: {
@@ -20,7 +21,11 @@ class LocationDetailsGrid extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            isFullSizeImageOpen: false
         };
+
+        this._onClickFullSizeImage = this._onClickFullSizeImage.bind(this);
+        this._triggerWindowClose = this._triggerWindowClose.bind(this);
     }
 
     render() {
@@ -33,14 +38,32 @@ class LocationDetailsGrid extends Component {
                     style={styles.gridList}
                 >
                     {this.props.locationDetails.images.map((image) => (
-                        <GridTile
-                            actionPosition="left"
-                            titlePosition="top"
-                            titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
-                            key={image}
-                        >
-                            <img src={image} />
-                        </GridTile>
+                        // <GridTile
+                        //     actionPosition="left"
+                        //     titlePosition="top"
+                        //     titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+                        //     key={image}
+                        // >
+                        <div>
+
+                        
+                            <img src={image} onClick={this._onClickFullSizeImage}/>
+                            <div>
+
+                            
+                                {this.state.isFullSizeImageOpen ?
+                                <FullSizeImage 
+                                    image={image}
+                                    isFullSizeImageOpen={this.state.isFullSizeImageOpen}
+                                    triggerWindowClose={this._triggerWindowClose}
+                                />
+                            :
+                                null
+                            }
+                            </div>
+                            
+                        </div>    
+                        // </GridTile>                      
                     ))}
 
                 </GridList>
@@ -48,6 +71,18 @@ class LocationDetailsGrid extends Component {
 
 
         );
+    }
+
+    _onClickFullSizeImage() {
+        this.setState({
+            isFullSizeImageOpen: !this.state.isFullSizeImageOpen
+        })
+    }
+
+    _triggerWindowClose() {
+        this.setState({
+            isFullSizeImageOpen: false
+        })
     }
 
 }
