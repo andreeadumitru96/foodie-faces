87fd02e208befcf4c3614441ce4013dce6c61416
Andreea <dumitru_andreea96@yahoo.com>
2018-06-20 20:19:40 +0200

    Client - Add API key for google maps

--- client/src/components/individual/LocationSearchContainer/GoogleMapContainer/PinPoint/PinPoint.jsx
+++ client/src/components/individual/LocationSearchContainer/GoogleMapContainer/PinPoint/PinPoint.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './PinPoint.css';
 
-const PinPoint = ({text}) => {
+const PinPoint = () => {
     return (
         <div className="pin-point-locationName">
             <i className="fa fa-map-marker"></i>
